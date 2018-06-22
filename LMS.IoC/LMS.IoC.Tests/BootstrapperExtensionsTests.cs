a2d32bc9d8198e079760780ed7d027eb1105c1ea
Milo Wical <Milo.Wical@compare.com>
2018-06-21 21:24:27 -0400

    Added inital Bootstrapper class. Still need to add more static methods.

---
+++ LMS.IoC/LMS.IoC.Tests/BootstrapperExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿namespace LMS.IoC.Tests
+{
+    using System;
+    using Admiral.Components.Instrumentation.Contract;
+    using Compare.Components.Notification.Contract;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BootstrapperExtensionsTests
+    {
+        private IServiceCollection _container;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _container = new ServiceCollection();
+        }
+
+        [TestMethod]
+        public void AddLoggerTest()
+        {
+            var provider = _container.AddLogger().BuildServiceProvider();
+
+            var logger = provider.GetService<ILogger>();
+            Assert.IsNotNull(logger);
+        }
+
+        [TestMethod]
+        public void AddNotificationChannelTest()
+        {
+            var provider = _container.AddNotificationChannel().BuildServiceProvider();
+
+            INotificationChannel<string> channel;
+
+            try
+            {
+                //Can't build without the ILogger dependency.
+                channel = provider.GetService<INotificationChannel<string>>();
+            }
+            catch (ArgumentNullException ane)
+            {
+                //Swallow the exception because we know what caused it.
+            }
+
+            provider = _container.AddLogger().BuildServiceProvider();
+
+            channel = provider.GetService<INotificationChannel<string>>();
+            Assert.IsNotNull(channel);
+        }
+
+        
+    }
+}
---
+++ LMS.IoC/LMS.IoC/Bootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.IoC
+{
+    using Admiral.Components.Instrumentation.Contract;
+    using Admiral.Components.Instrumentation.Logging;
+    using Compare.Components.Notification.Channels.InProc;
+    using Compare.Components.Notification.Contract;
+    using LeadEntity.Interface;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Bootstrapper
+    {
+        public static IServiceCollection BuildUp(this IServiceCollection container)
+        {
+            container
+                .AddLogger()
+                .AddNotificationChannel();
+
+            return container;
+        }
+
+        public static IServiceCollection AddLogger(this IServiceCollection container)
+        {
+            var logger = new Logger
+            {
+                AppDomainName = "Lead Management Service",
+                ProcessName = "Lead Management Service"
+            };
+
+            container.AddSingleton<ILogger>(logger);
+
+            return container;
+        }
+
+        public static IServiceCollection AddNotificationChannel(this IServiceCollection container)
+        {
+            container.AddSingleton<INotificationChannel<string>>(provider =>
+                new InProcNotificationChannel<string>("Lead Channel", provider.GetService<ILogger>()));
+
+            return container;
+        }
+
+        public static IServiceCollection
+    }
+}
