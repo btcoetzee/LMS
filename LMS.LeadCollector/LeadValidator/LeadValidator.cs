c9d02cf42655d3388d3f6118c0e32b0c2476d3ce
belinda.coetzee <belinda.coetzee@compare.com>
2018-06-25 12:20:17 -0400

    Worked on Validator

--- LMS.LeadCollector/LeadValidator/LeadValidator.cs
+++ LMS.LeadCollector/LeadValidator/LeadValidator.cs
@@ -1,4 +1,8 @@
-﻿namespace LeadValidator
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LeadValidator
 {
     using LeadEntity.Interface;
     using System;
@@ -9,7 +13,26 @@
     {
         public bool ValidLead(ILeadEntity lead)
         {
-            throw new NotImplementedException();
+            if (lead.Context.Length < 0)
+                return false;
+
+            if (lead.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ContextKeys.ActivityGuidKey) == null)
+            {
+                return false;
+            }
+            else
+            {
+                // Check the value here.
+                var guid = lead.Context.Where(ac => ac.Id == LeadEntity.Interface.Constants.ContextKeys.ActivityGuidKey)
+                    .Select(i => i.Value).ToString();
+                if (guid.ToString() == null || guid == Guid.Empty.ToString())
+                {
+                    return false;
+                }
+            }
+            return true;
+
+
         }
     }
 }
