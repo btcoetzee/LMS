d02e70b53e9d5a810cdd55b66cac206868f5d00c
Milo Wical <Milo.Wical@compare.com>
2018-07-02 15:48:30 -0400

    Added default logging object structs.

---
+++ LMS.LoggerClient/LMS.LoggerClient.Interface/DefaultLoggerClientErrorObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.LoggerClient.Interface
+{
+    public struct DefaultLoggerClientErrorObject : ILoggerClientErrorObject
+    {
+        public DateTime LogDateTime { get; set; }
+        public string SolutionContext { get; set; }
+        public string ProcessContext { get; set; }
+        public string OperationContext { get; set; }
+        public string ErrorContext { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
---
+++ LMS.LoggerClient/LMS.LoggerClient.Interface/DefaultLoggerClientObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.LoggerClient.Interface
+{
+    public struct DefaultLoggerClientObject : ILoggerClientObject
+    {
+        public DateTime LogDateTime { get; set; }
+        public string SolutionContext { get; set; }
+        public string ProcessContext { get; set; }
+        public string OperationContext { get; set; }
+    }
+}
