45fef1685c82c85455cefeaf3489b965f7e951c4
jess.curry <jess.curry@compare.com>
2018-07-03 14:44:12 -0400

    Added the new context in the components and added the decorator.

---
+++ LMS.LeadEntity/LMS.LeadEntity.Components/DefaultProperty.cs
@@ -0,0 +1,19 @@
+﻿using LMS.LeadEntity.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.LeadEntity.Components
+{
+    public struct DefaultProperty : IProperty
+    {
+        public string Id { get; }
+        public object Value { get; }
+
+        public DefaultProperty(string id, object value)
+        {
+            Id = id;
+            Value = value;
+        }
+    }
+}
