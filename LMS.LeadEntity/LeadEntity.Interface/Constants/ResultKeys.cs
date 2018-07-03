47fe602b4f87f9542dc0fe5b42104a0c88953239
jess.curry <jess.curry@compare.com>
2018-07-03 13:34:13 -0400

    Added the IResults and results to the lead entity.

---
+++ LMS.LeadEntity/LeadEntity.Interface/Constants/ResultKeys.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.LeadEntity.Interface.Constants
+{
+    public static class ResultKeys
+    {
+        public const string ResultTimeStampKey = "ResultTimeStamp";
+    }
+}
