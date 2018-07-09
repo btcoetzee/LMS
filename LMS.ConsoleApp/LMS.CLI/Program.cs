d9a3b4176dca40666180f1f0b3fc5193ad5599ca
Milo Wical <Milo.Wical@compare.com>
2018-07-09 13:29:29 -0400

    Updatred CLI app for a multi-threaded demo.

--- LMS.ConsoleApp/LMS.CLI/Program.cs
+++ LMS.ConsoleApp/LMS.CLI/Program.cs
@@ -2,6 +2,8 @@
 
 namespace LMS.CLI
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using IoC;
     using LeadCollector.Interface;
     using Microsoft.Extensions.DependencyInjection;
@@ -10,14 +12,92 @@ namespace LMS.CLI
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
 
-            var leadCollector = new ServiceCollection()
-                .BuildUp()
-                .BuildServiceProvider()
-                .GetService<ILeadCollector>();
+            //var leadCollector = new ServiceCollection()
+            //    .BuildUp()
+            //    .BuildServiceProvider()
+            //    .GetService<ILeadCollector>();
+
+            AsyncDemo("Hello World!");
+
+            for (var i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
 
             Console.ReadKey();
+        }
+        private static void AsyncDemo(string message)
+        {
+            var writer = new Writer(); //Akin to the Resolver
+
+            var random = new Random();
+
+            const int threadCount = 5;
+            var responses = new string[threadCount];
+
+            var task = new Task<string[]>(() =>
+            {
+                var campaigns = new Task<string>[threadCount]; //These are the campaign threads
+
+                for (var i = 0; i < threadCount; i++)
+                {
+                    var closure = i;
+                    var delay = random.Next(1500, 7500);
+
+                    campaigns[i] = new Task<string>(() =>
+                    {
+                        Thread.Sleep(delay);
+
+                        var color = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"{closure}: Done sleeping!");
+                        Console.ForegroundColor = color;
+
+                        if ((closure + 1) % 3 != 0)
+                            return $"Task {closure} processed message: \"{message}\" after {delay}ms";
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{closure}: Encountered an exception!");
+                        Console.ForegroundColor = color;
+
+                        return null;
+                    });
+
+                    campaigns[i].Start();
+                }
+
+                for (var i = 0; i < threadCount; i++)
+                {
+                    responses[i] = campaigns[i].Result;
+                }
+
+                return responses;
+            });
+
+            //This is the resolver step.
+            task.ContinueWith(async messageCollection => writer.WriteOut(await messageCollection));
+
+            task.Start();
+        }
+    }
+
+    internal class Writer
+    {
+        //This is where the resolver would do its work.
+        public void WriteOut(string[] messages)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            foreach (var m in messages)
+            {
+                Console.WriteLine(m);
+            }
+
+            Console.ForegroundColor = color;
         }
     }
 }
