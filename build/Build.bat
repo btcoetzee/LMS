70c0089b1f72e611102d606c1eb477b8cf986579
belinda.coetzee <belinda.coetzee@compare.com>
2018-07-16 10:55:34 -0400

    Updated Build File

--- build/Build.bat
+++ build/Build.bat
@@ -25,9 +25,9 @@ msbuild ..\LMS.Publisher\LMS.Publisher.sln >%OUTPUTFILE%
 type %OUTPUTFILE%  |  findstr /I /B /R  "Build" |findstr "succeeded." 
 if %ERRORLEVEL% NEQ 0 TYPE %OUTPUTFILE%  & ECHO "BUILD FAILED." & pause
 REM -------------------------------------------------
-REM LMS.LeadCollector
+REM LMS.LoggerClient
 REM -------------------------------------------------
-msbuild ..\LMS.LeadCollector\LMS.LeadCollector.sln >%OUTPUTFILE%  
+msbuild ..\LMS.LoggerClient\LMS.LoggerClient.sln >%OUTPUTFILE%  
 type %OUTPUTFILE%  |  findstr /I /B /R  "Build" |findstr "succeeded." 
 if %ERRORLEVEL% NEQ 0 TYPE %OUTPUTFILE%  & ECHO "BUILD FAILED." & pause
 REM -------------------------------------------------
@@ -37,15 +37,21 @@ msbuild ..\LMS.Resolution\LMS.Resolution.sln >%OUTPUTFILE%
 type %OUTPUTFILE%  |  findstr /I /B /R  "Build" |findstr "succeeded." 
 if %ERRORLEVEL% NEQ 0 TYPE %OUTPUTFILE%  & ECHO "BUILD FAILED." & pause
 REM -------------------------------------------------
-REM LMS.LoggerClient
+REM LMS.LeadCollector
 REM -------------------------------------------------
-msbuild ..\LMS.Resolution\LMS.LoggerClient.sln >%OUTPUTFILE%  
+msbuild ..\LMS.LeadCollector\LMS.LeadCollector.sln >%OUTPUTFILE%  
 type %OUTPUTFILE%  |  findstr /I /B /R  "Build" |findstr "succeeded." 
 if %ERRORLEVEL% NEQ 0 TYPE %OUTPUTFILE%  & ECHO "BUILD FAILED." & pause
 REM -------------------------------------------------
 REM LMS.Campaign
 REM -------------------------------------------------
 msbuild ..\LMS.Campaign\LMS.Campaign.sln >%OUTPUTFILE%  
+type %OUTPUTFILE%  |  findstr /I /B /R  "Build" |findstr "succeeded." 
+if %ERRORLEVEL% NEQ 0 TYPE %OUTPUTFILE%  & ECHO "BUILD FAILED." & pause
+REM -------------------------------------------------
+REM LMS.CampaignManager
+REM -------------------------------------------------
+msbuild ..\LMS.CampaignManager\LMS.CampaignManager.sln >%OUTPUTFILE%  
 type %OUTPUTFILE%  |  findstr /I /B /R  "Build" |findstr "succeeded." 
 if %ERRORLEVEL% NEQ 0 TYPE %OUTPUTFILE%  & ECHO "BUILD FAILED." & pause
 
