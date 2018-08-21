3ac7e6fd0df2a22a71ba97b73eb1c884250aacca
jess.curry <jess.curry@compare.com>
2018-08-21 10:56:19 -0400

    Changed the name to capital Has and added valid to function name

--- LMS.Control.Validators/LMS.Control.Validators/ValidatorCollection.cs
+++ LMS.Control.Validators/LMS.Control.Validators/ValidatorCollection.cs
@@ -6,7 +6,7 @@
 
     public class ValidatorCollection
     {
-        public bool hasActivityGuid(ILeadEntity leadEntity)
+        public bool HasValidActivityGuid(ILeadEntity leadEntity)
         {
 
             var activityGuidValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ContextKeys.ActivityGuidKey)?.Value;
@@ -18,7 +18,7 @@
                 return true;
         }
 
-        public bool hasSessionGuid(ILeadEntity leadEntity)
+        public bool HasValidSessionGuid(ILeadEntity leadEntity)
         {
             var sessionGuidValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ContextKeys.SessionGuidKey)?.Value;
             if ((sessionGuidValue == null) || (!Guid.TryParse(sessionGuidValue.ToString(), out Guid sessionGuid)))
@@ -29,7 +29,7 @@
             return true;
         }
 
-        public bool hasIdentityGuid(ILeadEntity leadEntity)
+        public bool HasValidIdentityGuid(ILeadEntity leadEntity)
         {
             var identityGuidValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ContextKeys.IdentityGuidKey)?.Value;
             if ((identityGuidValue == null) || (!Guid.TryParse(identityGuidValue.ToString(), out Guid identityGuid)))
@@ -40,7 +40,7 @@
             return true;
         } 
 
-        public bool hasQuotedProduct(ILeadEntity leadEntity)
+        public bool HasValidQuotedProduct(ILeadEntity leadEntity)
         {
             var quotedProductValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ContextKeys.QuotedProductKey)?.Value;
             if ((quotedProductValue == null) || (!Int32.TryParse(quotedProductValue.ToString(), out int quotedProduct)))
@@ -51,7 +51,7 @@
             return true;
         }
 
-        public bool hasAdditionalProducts(ILeadEntity leadEntity)
+        public bool HasValidAdditionalProducts(ILeadEntity leadEntity)
         {
             var additionalProductValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ContextKeys.AdditionalProductKey)?.Value;
             if ((additionalProductValue == null) || (!Int32.TryParse(additionalProductValue.ToString(), out int additionalProduct)))
@@ -62,7 +62,7 @@
             return true;
         }
 
-        public bool hasPriorInsurance(ILeadEntity leadEntity)
+        public bool HasValidPriorInsurance(ILeadEntity leadEntity)
         {
             var priorInsuranceValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.PriorInsuranceKey)?.Value;
             if ((priorInsuranceValue == null) || (!bool.TryParse(priorInsuranceValue.ToString(), out bool priorInsurance)))
@@ -73,7 +73,7 @@
             return true;
         }
 
-        public bool hasPriorBI(ILeadEntity leadEntity)
+        public bool HasValidPriorBI(ILeadEntity leadEntity)
         {
             var priorBIValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.PriorBIKey)?.Value;
             if ((priorBIValue == null) || (!Int32.TryParse(priorBIValue.ToString(), out int priorBI)))
@@ -84,7 +84,7 @@
             return true;
         }
 
-        public bool hasVehicleCount(ILeadEntity leadEntity)
+        public bool HasValidVehicleCount(ILeadEntity leadEntity)
         {
             var vehicleCountValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.VehicleCountKey)?.Value;
             if ((vehicleCountValue == null) || (!Int32.TryParse(vehicleCountValue.ToString(), out int vehicleCount)))
@@ -95,7 +95,7 @@
             return true;
         }
 
-        public bool hasPNIAge(ILeadEntity leadEntity)
+        public bool HasValidPNIAge(ILeadEntity leadEntity)
         {
             var pniAgeValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.PNI_Age)?.Value;
             if ((pniAgeValue == null) || (!Int32.TryParse(pniAgeValue.ToString(), out int pniAge)))
@@ -106,7 +106,7 @@
             return true;
         }
 
-        public bool hasQuotedBI(ILeadEntity leadEntity)
+        public bool HasValidQuotedBI(ILeadEntity leadEntity)
         {
             var quotedBIValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.QuotedBIKey)?.Value;
             if ((quotedBIValue == null) || (!Int32.TryParse(quotedBIValue.ToString(), out int quotedBI)))
@@ -117,7 +117,7 @@
             return true;
         }
 
-        public bool hasPhoneNumber(ILeadEntity leadEntity)
+        public bool HasValidPhoneNumber(ILeadEntity leadEntity)
         {
             var phoneNumberValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.PhoneNumber)?.Value;
             if ((phoneNumberValue == null) || (!Int32.TryParse(phoneNumberValue.ToString(), out int phoneNumber)))
@@ -128,7 +128,7 @@
             return true;
         }
 
-        public bool hasHighPOP(ILeadEntity leadEntity)
+        public bool HasValidHighPOP(ILeadEntity leadEntity)
         {
             var highPOPValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.SegementKeys.HighPOPKey)?.Value;
             if ((highPOPValue == null) || (!Int32.TryParse(highPOPValue.ToString(), out int highPOP)))
@@ -139,7 +139,7 @@
             return true;
         }
 
-        public bool hasHomeOwner(ILeadEntity leadEntity)
+        public bool HasValidHomeOwner(ILeadEntity leadEntity)
         {
             var homeOwnerValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.SegementKeys.HomeownerKey)?.Value;
             if ((homeOwnerValue == null) || (!Int32.TryParse(homeOwnerValue.ToString(), out int homeOwner)))
@@ -150,7 +150,7 @@
             return true;
         }
 
-        public bool hasSessionRequestSeq(ILeadEntity leadEntity)
+        public bool HasValidSessionRequestSeq(ILeadEntity leadEntity)
         {
             var sessionRequestSeqValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ContextKeys.SessionRequestSeqKey)?.Value;
             if ((sessionRequestSeqValue == null) || (!Int32.TryParse(sessionRequestSeqValue.ToString(), out int sessionRequestSeq)))
@@ -161,7 +161,7 @@
             return true;
         }
 
-        public bool hasSiteID(ILeadEntity leadEntity)
+        public bool HasValidSiteID(ILeadEntity leadEntity)
         {
             var siteIDValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ContextKeys.SiteIDKey)?.Value;
             if ((siteIDValue == null) || (!Int32.TryParse(siteIDValue.ToString(), out int siteId)))
@@ -172,7 +172,7 @@
             return true;
         }
 
-        public bool hasEnvironment(ILeadEntity leadEntity)
+        public bool HasValidEnvironment(ILeadEntity leadEntity)
         {
             var environmentValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ContextKeys.EnvironmentKey)?.Value;
             if ((environmentValue == null) || (!Int32.TryParse(environmentValue.ToString(), out int environment)))
@@ -183,7 +183,7 @@
             return true;
         }
 
-        public bool hasEmailAddress(ILeadEntity leadEntity)
+        public bool HasValidEmailAddress(ILeadEntity leadEntity)
         {
             var emailAddressValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.EmailAddressKey)?.Value;
             if ((emailAddressValue == null) || (!Int32.TryParse(emailAddressValue.ToString(), out int emailAddress)))
@@ -194,7 +194,7 @@
             return true;
         }
 
-        public bool hasState(ILeadEntity leadEntity)
+        public bool HasValidState(ILeadEntity leadEntity)
         {
             var stateValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.StateKey)?.Value;
             if ((stateValue == null) || (!Int32.TryParse(stateValue.ToString(), out int state)))
@@ -205,7 +205,7 @@
             return true;
         }
 
-        public bool hasFullName(ILeadEntity leadEntity)
+        public bool HasValidFullName(ILeadEntity leadEntity)
         {
             var fullNameValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.FullNameKey)?.Value;
             if ((fullNameValue == null) || (!Int32.TryParse(fullNameValue.ToString(), out int fullName)))
@@ -216,7 +216,7 @@
             return true;
         }
 
-        public bool hasAddress(ILeadEntity leadEntity)
+        public bool HasValidAddress(ILeadEntity leadEntity)
         {
             var addressValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.PropertyKeys.AddressKey)?.Value;
             if ((addressValue == null) || (!Int32.TryParse(addressValue.ToString(), out int address)))
@@ -227,7 +227,7 @@
             return true;
         }
 
-        public bool hasLowPOP(ILeadEntity leadEntity)
+        public bool HasValidLowPOP(ILeadEntity leadEntity)
         {
             var lowPOPValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.SegementKeys.LowPOPKey)?.Value;
             if ((lowPOPValue == null) || (!Int32.TryParse(lowPOPValue.ToString(), out int lowPOP)))
@@ -238,7 +238,7 @@
             return true;
         }
 
-        public bool hasRenter(ILeadEntity leadEntity)
+        public bool HasValidRenter(ILeadEntity leadEntity)
         {
             var renterValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.SegementKeys.RenterKey)?.Value;
             if ((renterValue == null) || (!Int32.TryParse(renterValue.ToString(), out int renter)))
@@ -249,7 +249,7 @@
             return true;
         }
 
-        public bool hasQuoteRef(ILeadEntity leadEntity)
+        public bool HasValidQuoteRef(ILeadEntity leadEntity)
         {
             var quoteRefValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ActivityKeys.QuoteReferenceKey)?.Value;
             if ((quoteRefValue == null) || (!Int32.TryParse(quoteRefValue.ToString(), out int quoteRef)))
@@ -260,7 +260,7 @@
             return true;
         }
 
-        public bool hasAnnualPremium(ILeadEntity leadEntity)
+        public bool HasValidAnnualPremium(ILeadEntity leadEntity)
         {
             var annualPremiumValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ActivityKeys.AnnualPremiumKey)?.Value;
             if ((annualPremiumValue == null) || (!Int32.TryParse(annualPremiumValue.ToString(), out int annualPremium)))
@@ -271,7 +271,7 @@
             return true;
         }
 
-        public bool hasDownPayment(ILeadEntity leadEntity)
+        public bool HasValidDownPayment(ILeadEntity leadEntity)
         {
             var downPaymentValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ActivityKeys.DownPaymentKey)?.Value;
             if ((downPaymentValue == null) || (!Int32.TryParse(downPaymentValue.ToString(), out int downPayment)))
@@ -282,7 +282,7 @@
             return true;
         }
 
-        public bool hasMonthlyInstallment(ILeadEntity leadEntity)
+        public bool HasValidMonthlyInstallment(ILeadEntity leadEntity)
         {
             var monthlyInstallmentValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ActivityKeys.MonthlyInstallmentKey)?.Value;
             if ((monthlyInstallmentValue == null) || (!Int32.TryParse(monthlyInstallmentValue.ToString(), out int monthlyInstallment)))
@@ -293,7 +293,7 @@
             return true;
         }
 
-        public bool hasRequote(ILeadEntity leadEntity)
+        public bool HasValidRequote(ILeadEntity leadEntity)
         {
             var requoteValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ActivityKeys.RequoteKey)?.Value;
             if ((requoteValue == null) || (!Int32.TryParse(requoteValue.ToString(), out int requote)))
@@ -304,7 +304,7 @@
             return true;
         }
 
-        public bool hasBuyOnlineClick(ILeadEntity leadEntity)
+        public bool HasValidBuyOnlineClick(ILeadEntity leadEntity)
         {
             var buyOnlineClickValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ActivityKeys.BuyOnlineClickKey)?.Value;
             if ((buyOnlineClickValue == null) || (!Int32.TryParse(buyOnlineClickValue.ToString(), out int buyOnlineClick)))
@@ -315,7 +315,7 @@
             return true;
         }
 
-        public bool hasBuyOnlineBrand(ILeadEntity leadEntity)
+        public bool HasValidBuyOnlineBrand(ILeadEntity leadEntity)
         {
             var buyOnlineBrandValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ActivityKeys.BuyOnlineBrandKey)?.Value;
             if ((buyOnlineBrandValue == null) || (!Int32.TryParse(buyOnlineBrandValue.ToString(), out int buyOnlineBrand)))
@@ -326,7 +326,7 @@
             return true;
         }
 
-        public bool hasBuyByPhoneClick(ILeadEntity leadEntity)
+        public bool HasValidBuyByPhoneClick(ILeadEntity leadEntity)
         {
             var buyByPhoneClickValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ActivityKeys.BuyByPhoneClickKey)?.Value;
             if ((buyByPhoneClickValue == null) || (!Int32.TryParse(buyByPhoneClickValue.ToString(), out int buyByPhoneClick)))
@@ -337,7 +337,7 @@
             return true;
         }
 
-        public bool hasBuyByPhoneBrand(ILeadEntity leadEntity)
+        public bool HasValidBuyByPhoneBrand(ILeadEntity leadEntity)
         {
             var buyByPhoneBrandValue = leadEntity.Context.SingleOrDefault(item => item.Id == LeadEntity.Interface.Constants.ActivityKeys.BuyByPhoneBrandKey)?.Value;
             if ((buyByPhoneBrandValue == null) || (!Int32.TryParse(buyByPhoneBrandValue.ToString(), out int buyByPhoneBrand)))
